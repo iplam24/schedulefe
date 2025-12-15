9f541d8c4566b63243e70790506660b4c72b0866
iplam24 <vxlcontact143@gmail.com>
2025-12-15 20:07:25 +0700

    server vip

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    
   ],
-
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://160.30.192.113:8090',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
